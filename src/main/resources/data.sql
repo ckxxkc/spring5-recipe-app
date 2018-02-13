34eb2b790df27da75ad915bd2d660e4a386ca0c5
cornelia.sarbu <cS$1104+>
2018-02-13 13:21:38 +0200

    Db initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT into category (description) VALUES ('American');
+INSERT into category (description) VALUES ('Italian');
+INSERT into category (description) VALUES ('Mexican');
+INSERT into category (description) VALUES ('Fast Food');
+INSERT into unit_of_measure (description) VALUES ('Teaspoon');
+INSERT into unit_of_measure (description) VALUES ('Tablespoon');
+INSERT into unit_of_measure (description) VALUES ('Cup');
+INSERT into unit_of_measure (description) VALUES ('Pinch');
+INSERT into unit_of_measure (description) VALUES ('Ounce');
+INSERT into unit_of_measure (description) VALUES ('Each');
+INSERT into unit_of_measure (description) VALUES ('Dash');
+INSERT into unit_of_measure (description) VALUES ('Pint');
