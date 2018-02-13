d1d8c7b31fa4055c42abda13f8201f3d09674ef7
cornelia.sarbu <cS$1104+>
2018-02-13 15:52:43 +0200

    receipeBootstrap

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,7 +1,6 @@
 package guru.springframework.repositories;
 
 import guru.springframework.domain.Category;
-import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
